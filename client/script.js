e2bcce7f04b51159a7b151345c33b22f21da1e78
Katya Heylen <ezhgurova@gmail.com>
2021-12-02 16:50:51 +0100

    chat works without css

--- client/script.js
+++ client/script.js
@@ -1,3 +1,31 @@
 let socket = io.connect();
-
 console.log('hi from the script.js file');
+
+document.getElementById('message').addEventListener('keyup', function(event) {
+    if (event.code === 'Enter') {
+        event.preventDefault();
+        document.getElementById('send').click();
+    }
+});
+
+document.getElementById('message').addEventListener('keyup', function(event) {
+    if (event.code === 'Enter' && event.code === 'ControlLeft') {
+        event.preventDefault();
+        document.getElementById('private').click();
+    }
+});
+
+
+document.getElementById("send").addEventListener('click', () => {
+    let message = document.getElementById('message').value;
+    socket.emit('sendToAll', (message));
+});
+
+socket.on('displayMessage', (message) => {
+    let target = document.getElementById('target');
+    target.innerHTML += '<br>'+ message;
+});
+document.getElementById("private").addEventListener('click', () => {
+    let message = document.getElementById('message').value;
+    socket.emit('sendToMe', (message));
+});
--- server/server.js
+++ server/server.js
@@ -5,12 +5,11 @@ const app = express();
 const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 server.listen(8080, () => {
     console.log("server running on " + 8080);
 });
-
-const io = require('socket.io')(server);
 
 let counter = 0;
 
@@ -19,7 +18,11 @@ io.on('connection', (socket) => {
     console.log(counter + ' someone connected');
 
     socket.on('sendToAll', (message) => {
+        console.log(message);
         io.emit("displayMessage", (message));
     });
-
+    socket.on('sendToMe', (message) => {
+        console.log(message);
+        socket.emit("displayMessage", (message));
+    });
 });
