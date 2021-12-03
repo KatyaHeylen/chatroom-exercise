08f3bd2f1c1c4530debdcf3fd65fa74b64dae420
Katya Heylen <ezhgurova@gmail.com>
2021-12-03 16:16:31 +0100

    must have features are completed and a bit more

--- client/script.js
+++ client/script.js
@@ -1,5 +1,7 @@
 let socket = io.connect();
-console.log('hi from the script.js file');
+
+let name = prompt("What is your nickname?", "");
+socket.emit('login', name);
 
 document.getElementById('message').addEventListener('keyup', function(event) {
     if (event.code === 'Enter') {
@@ -15,17 +17,34 @@ document.getElementById('message').addEventListener('keyup', function(event) {
     }
 });
 
-
 document.getElementById("send").addEventListener('click', () => {
     let message = document.getElementById('message').value;
-    socket.emit('sendToAll', (message));
+    socket.emit('sendToAll', name, message);
+    document.getElementById("message").value = "";
 });
 
-socket.on('displayMessage', (message) => {
+socket.on('displayMessage', (name, message) => {
+    let today = new Date();
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes();
+    let dayTime = date + ": " + time;
     let target = document.getElementById('target');
-    target.innerHTML += '<br>'+ message;
+    target.innerHTML += '<br>' + dayTime + " " + name +': ' + message;
 });
 document.getElementById("private").addEventListener('click', () => {
     let message = document.getElementById('message').value;
-    socket.emit('sendToMe', (message));
+    socket.emit('sendToMe', name, message);
 });
+
+socket.on('displayList', (names) => {
+    document.getElementById("list").innerHTML = '';
+    names.forEach(name => {
+        document.getElementById("list").innerHTML += "<br>" + name;
+    })
+})
+
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+let names = [];
 
 server.listen(8080, () => {
     console.log("server running on " + 8080);
@@ -15,14 +16,18 @@ let counter = 0;
 
 io.on('connection', (socket) => {
     counter++;
-    console.log(counter + ' someone connected');
+    console.log(counter + ' connected');
 
-    socket.on('sendToAll', (message) => {
-        console.log(message);
-        io.emit("displayMessage", (message));
+    socket.on('sendToAll', (name, message) => {
+        console.log(name, message);
+        io.emit("displayMessage", name, message);
     });
-    socket.on('sendToMe', (message) => {
-        console.log(message);
-        socket.emit("displayMessage", (message));
+    socket.on('sendToMe', (name, message) => {
+        console.log(name, message);
+        socket.emit("displayMessage", name, message);
     });
+    socket.on('login', (name) => {
+        names.push(name);
+        socket.emit("displayList", names);
+    })
 });
